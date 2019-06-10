34c1aacf3c881c7a4ddd269404fa51983c47ef1c
elespdn <spadinielena@gmail.com>
2019-06-10 19:48:19 +0200

    fix script to convert Tracer output

--- tracerMacroAl/README.md
+++ tracerMacroAl/README.md
@@ -4,6 +4,11 @@
 
 Macro-alignment is useful to identify corresponding sections to collate.
 
+## This repo
+- `reorderTracerTable4collation.py` takes Tracer output table and reorder it, moving all aligned sections to the same row
+- 'roudTracer.csv' is an example of Tracer output table
+- 'roudTracer_onlyRelevantData.csv' and 'roudTracer_onlyRelevantData_reordered.csv' are created by the script. The final result is the second. The cell of each row correspond to the witnesses and can be collated.
+
 
 ## Steps
 1. Run Tracer and output results in tabular format, see [Tracer manual](https://gfranzini.gitbooks.io/tracer/content/beta/results-as-csv.html). For preparing the data for Tracer follow the manual; some [Python scripts](https://github.com/mikekestemont/potter/blob/master/harry/intertextuality/intertextuality.ipynb) and [R scripts](https://github.com/editio/tracer-scripts) are also available
@@ -15,11 +20,5 @@ Go to step 3 above: run `reorderTracerTable4collation.py` on the csv provided 'r
 
 
 ## Is this useful?
-Test with Alexis lemmatized with Pyrrha: shuffle two verses and give to Tracer to identify --> only one identified; best parameters: results score 0.2, frequency 0.8. Probably better on longer units, such as paragraphs!
+Test with *Alexis* lemmatized with Pyrrha: shuffle two verses and give to Tracer to identify --> only one identified; best parameters: results score 0.2, frequency 0.8. Probably better on longer units, such as paragraphs! Test further ...
 
-
----
-
-### *work in progress*
-
----
--- tracerMacroAl/reorderTracerTable4collation.py
+++ tracerMacroAl/reorderTracerTable4collation.py
@@ -1,61 +1,71 @@
 
 import csv
-import pandas as pd
-
 
 # from Tracer output create table with only identifiers and texts
 with open("roudTracer.csv", 'r') as in_file, open("roudTracer_onlyRelevantData.csv", 'w') as out_file:
 	outdata = []
 	reader = csv.reader(in_file, delimiter="\t")
 	writer = csv.writer(out_file, delimiter="\t")
+	'''
+	for each row put together the first text (col4) and its index (col0)
+	and the second text (col5) and its index (col1). These cols are in Tracer output
+	'''
 	for row in reader:
 		outdata.append([row[0] + ". " + row[4], row[1] + ". " + row[5]])
 	writer.writerows(outdata)
 
-# create list of all cells
-with open("roudTracer_onlyRelevantData.csv", 'r') as in_file:
-	reader = csv.reader(in_file, delimiter="\t")
-	listAllCells = []
-	for idx, row in enumerate(reader):
-	    rowDict={}
-	    i = idx
-	    A = row[0]
-	    B = row[1]
-	    rowDict["i"]=i
-	    rowDict["a"]=A
-	    rowDict["b"]=B
-	    listAllCells.append(rowDict)
 
+####################################
+#
+# NEED TO SORT FIRST??
+#
+####################################
 
 # reorder table
-with open("roudTracer_onlyRelevantData.csv", 'r') as in_file, open("roudTracer_onlyRelevantData_FINAL.csv", 'w') as out_file:
+with open("roudTracer_onlyRelevantData.csv", 'r') as in_file, open("roudTracer_onlyRelevantData_reordered.csv", 'w') as out_file:
 	reader = csv.reader(in_file, delimiter="\t")
 	writer = csv.writer(out_file, delimiter="\t")
-	newFile = []
+	
+	# create an empty list
+	reorderedList = []
 	for idx, row in enumerate(reader):
-		A = row[0]
-		B = row[1]
-		# how many times A occurs. Literally, how long is the list of occurrences
-		Aocc = [element for element in listAllCells if (element['a'] == A or element['b'] == A)]
-		Bocc = [element for element in listAllCells if (element['a'] == B or element['b'] == B)]
-		if (len(Aocc) < 2 and len(Bocc) < 2):
-			newFile.append(row)
+		'''
+		each row is like this: ['text1', 'text2']
+		function adds index (idx) to each row
+		A takes value of text1 and B takes value of text2
+		'''
+		i = idx    
+		A = row[0] 
+		B = row[1] 
+		'''
+		A and B are added to the reorderedList only if they aren't there already, using 'not in'
+		The index is added only if A and B are both added.
+		If only A or only B are added, their row index is not added, so they follow the previous index.
+		Indexes will be used below to create the new lines
+		'''
+		# add new index only if new ('else'), otherwise will be added to previous one
+		if (A in reorderedList and B not in reorderedList):
+			reorderedList.append(B)
+		elif (B in reorderedList and A not in reorderedList):
+			reorderedList.append(A)
+		elif (A in reorderedList and B in reorderedList):
+			continue
+		else: # if A and B not in reorderedList
+			reorderedList.append(i)
+			reorderedList.append(A)
+			reorderedList.append(B)
+
+#this is not efficient, creates useless duplicates
+	newFile = []
+	for x in reorderedList:
+		if type(x) == int:
+			newLine = []
+			newLine.append(x)
 		else:
-			ArepItems = []
-			BrepItems = []
-			for x in Aocc:
-				ArepItems.append(x['a'])
-				BrepItems.append(x['b'])
-			newLineAocc = set(ArepItems + BrepItems)
-			
-			for x in Bocc:
-				ArepItems.append(x['a'])
-				BrepItems.append(x['b'])
-			newLineBocc = set(ArepItems + BrepItems)
-			newLine = (newLineAocc & newLineBocc) # merge sets only distinct
-			newFile.append(newLine)
-			
-	#deduplicate newFile
+			newLine.append(x)
+		newFile.append(newLine)
+	
+#deduplicate newFile
 	seen = set() # 
 	for row in newFile:
 		row = tuple(row)
@@ -63,8 +73,6 @@ with open("roudTracer_onlyRelevantData.csv", 'r') as in_file, open("roudTracer_o
 		seen.add(row)
 		writer.writerow(row)
 
-
-# iterate over reorder table
 
 
 
