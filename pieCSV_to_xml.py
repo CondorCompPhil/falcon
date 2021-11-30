c1dcba445572e76695b805d0a3f2988ab2eb5161
Jean-Baptiste Camps <jbcamps@hotmail.com>
2021-11-30 14:53:32 +0100

    merging

--- pieCSV_to_xml.py
+++ pieCSV_to_xml.py
@@ -1,6 +1,9 @@
 import sys
 from falcon import *
 import falcon.lemmatise_pie as lemmatise_pie
+import regex as re
+
+_FINALPUNCT = re.compile(r"[.?!]")
 
 if __name__ == "__main__":
     content = {}
@@ -12,9 +15,16 @@ if __name__ == "__main__":
         with open(file, 'r') as f:
             # pop first line (header)
             tasks = f.readline().rstrip().split('\t')
+            tasks = [t.lower() for t in tasks if t in ['token', 'form', 'pos', 'POS', 'lemma', 'morph'] ]
             sent = []
             for line in f.readlines():
                 t = line.rstrip().split('\t')
+                
+                if len(t) < len(tasks):
+                    print("Error on this line:")
+                    print(t)
+                    break
+                
                 if not t == ['']:
                     token_dict =  {"form": t[0], "id": "w_"+str(tokenId), "order_id": str(tokenId)}
                     # and now add the different annotations from lemmatiser
@@ -23,10 +33,17 @@ if __name__ == "__main__":
                     
                     sent.append(token_dict)
                     tokenId += 1
+                                
+                    if _FINALPUNCT.match(t[0]):
+                        content[wit].append(sent)
+                        sent = []
 
                 else:
                     content[wit].append(sent)
                     sent = []
+                
+            #Deal with last sentence, if file does not end properly
+            content[wit].append(sent)
 
     documents = lemmatise_pie.xmlify(content)
     for doc in documents:
